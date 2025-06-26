1f5f0ca0beb153c1c8b1e8ae1d51d03a3c7d23d7
Vitaliy Dorokhov <4810672@gmail.com>
2025-06-26 23:07:24 +0300

    change Home_Work_22.py

--- Home_Work_22.py
+++ Home_Work_22.py
@@ -1,52 +1,46 @@
-def read_cookbook(filename):
-    cook_book = {}
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            dish = line.strip()
-            if not dish:
-                continue
-            count = int(file.readline())
-            cook_book[dish] = []
-            for _ in range(count):
-                ing, qty, measure = file.readline().strip().split(' | ')
-                cook_book[dish].append({
-                    'ingredient_name': ing,
-                    'quantity': int(qty),
-                    'measure': measure
-                })
-            file.readline()
-        return cook_book
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
 
-cook_book = read_cookbook('recipes.txt')
-
-# from pprint import pprint
-# pprint(cook_book)
 
 def get_shop_list_by_dishes(dish_list, person_count):
     shop_list = {}
     for dish in dish_list:
         if dish in cook_book:
-            new_dish_list = dict('ingredient_name')
-            for ingredient in dish_list:
+            # new_dish_list = dict(dish)
+            for ingredient in cook_book[dish]:
+                new_dish_list = dict(ingredient)
+
                 new_dish_list['quantity'] = new_dish_list['quantity'] * person_count
-                if new_dish_list['ingredient_name'] in shop_list:
-                    shop_list[new_dish_list['ingridient_name']]['quantity'] += new_dish_list['quantity']
+                if new_dish_list['ingredient_name'] not in shop_list:
+                    shop_list[new_dish_list['ingredient_name']] = new_dish_list
                 else:
-                    shop_list[new_dish_list['ingridient_name']] = new_dish_list
+                    shop_list[new_dish_list['ingredient_name']]['quantity'] += new_dish_list['quantity']
 
     else:
         print('Нет такого блюда')
 
     return shop_list
 
+dish_list = ['Запеченный картофель']
+person_count = 3
+shopping_list = get_shop_list_by_dishes(dish_list, person_count)
 
-# from pprint import pprint
-# pprint(cook_book)
-
-
-dishes = ['Омлет', 'Фахитос']
-persons = 3
-shopping_list = get_shop_list_by_dishes(dishes, persons)
 
 
 
